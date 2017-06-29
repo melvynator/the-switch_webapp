98050f7b3b0a9c354fb9853f50cc610bbf9de386
Melvyn Peignon <peignonmel@eisti.eu>
2017-06-29 12:51:44 +0800

    Define server name

--- TheSwitchWebApp/__init__.py
+++ TheSwitchWebApp/__init__.py
@@ -38,7 +38,6 @@ app.register_blueprint(user_module)
 app.register_blueprint(register)
 
 
-app.config['SERVER_NAME'] = "http://secure-atoll-58141.herokuapp.com"
 # Generate the key
 os.environ["SECRET_KEY"] = str(SecureMessage().generate_key(), 'iso-8859-1')
 #print(config.SECRET_KEY)
